2f6021d20633a62ed6a2910b3a3184770442e76b
Debdeep Kundu <157905501+DebdeepKundu002@users.noreply.github.com>
2024-11-22 22:20:51 +0530

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import Login from './Login';
+// import Gender from './Gender';
+// import Abc from './Abc';
+// import Menu from './Menu';
+// import AddContact from './AddContact';
+// import Loginone from './Loginone';
+// import NewUser from './NewUser';
+// import ForgetPassword from './ForgetPassword';
+// import Dashboard from './Dashboard';
+// import Profile from './Profile';
+// import Exam from './Exam';
+import BloodyUserRegistration from './BloodyUserRegistration';
+import DonerUser from './DonerUser';
+import DonerUserRegistration from './DonerUserRegistration';
+// import UserDashboard from './UserDashboard';
+import Search from './Search';
+// import Searchone from './Searchone';
+import Homeone from './Homeone';
+import Slider from './Slider';
 import Main from './Main';
+import BloodUser from './Blooduser';
+// import AddProduct from './AddProduct';
+// import CartappHome from './CartappHome';
+import Payment from './Payment';
+import Demo from './Demo';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Main></Main>
+   <Main></Main> 
+   {/* <Payment></Payment> */}
+   {/* <Demo></Demo> */}
+  
   </React.StrictMode>
 );
 
